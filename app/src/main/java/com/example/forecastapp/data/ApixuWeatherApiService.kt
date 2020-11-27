40882247ace9fe55bc81ceb7da5e9f9dfabd2c70
AnanasName <56173538+AnanasName@users.noreply.github.com>
2020-11-27 14:09:29 +0300

    Добавлена RoomDatabase и Retrofit

---
+++ app/src/main/java/com/example/forecastapp/data/ApixuWeatherApiService.kt
@@ -0,0 +1,54 @@
+package com.example.forecastapp.data
+
+import com.example.forecastapp.data.response.CurrentWeatherResponse
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+const val API_KEY = "23933be25b231e09433b4a6cf8626894"
+//http://api.weatherstack.com/current?access_key=23933be25b231e09433b4a6cf8626894&query=London
+
+interface ApixuWeatherApiService {
+
+    @GET("current")
+    fun getCurrentWeather(
+        @Query("query") location:String): Deferred<CurrentWeatherResponse>
+
+    companion object{
+        operator fun invoke(): ApixuWeatherApiService{
+            val requestInterceptor = Interceptor{chain ->
+
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl("http://api.weatherstack.com/")
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApixuWeatherApiService::class.java)
+        }
+    }
+
+}
